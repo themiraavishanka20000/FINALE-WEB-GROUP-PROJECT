02d91179f1ee05985e2be8d5de286f2c28ec2843
nethminimthara2001 <87576598+nethminimthara2001@users.noreply.github.com>
2021-08-31 00:12:17 +0530

    Change navigation

---
+++ Button.js
@@ -0,0 +1,21 @@
+ function page1(){
+      location.replace("new.html");
+     } 
+function page2(){
+      location.replace("new.html");
+     } 
+function page3(){
+      location.replace("new.html");
+     } 
+function page4(){
+      location.replace("new.html");
+     } 
+function page5(){
+      location.replace("new.html");
+     } 
+function page6(){
+      location.replace("new.html");
+     } 
+function page7(){
+      location.replace("new.html");
+     } 
