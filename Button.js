4082ff340160e22b1ccddbacd7e03a831a33f520
Madushanka23 <88953682+Madushanka23@users.noreply.github.com>
2021-08-31 22:41:10 -0800

    adding navi bar

---
+++ Button.js
@@ -0,0 +1,21 @@
+ function page1(){
+      location.replace("test.html");
+     } 
+function page2(){
+      location.replace("test.html");
+     } 
+function page3(){
+      location.replace("test.html");
+     } 
+function page4(){
+      location.replace("test.html");
+     } 
+function page5(){
+      location.replace("test.html");
+     } 
+function page6(){
+      location.replace("test.html");
+     } 
+function page7(){
+      location.replace("test.html");
+     } 
