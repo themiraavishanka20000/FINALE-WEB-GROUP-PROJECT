e9806e0f46f7b4df98d21ecd5a2dcd5d8795c65d
Themira edirisinghe 2000 <donediridisinghe2000@gmail.com>
2021-09-10 01:31:53 +0530

    main first

---
+++ Button.js
@@ -0,0 +1,23 @@
+
+     function page1(){
+      location.replace("index.html");
+     } 
+     function page2(){
+      location.replace("products.html");
+     } 
+     function page3(){
+      location.replace("Servise.html");
+     } 
+     function page4(){
+      location.replace("companies.html");
+     } 
+     function page5(){
+      location.replace("Oraganizations.html");
+     } 
+     function page6(){
+      location.replace("Eventpage.html");
+     } 
+     function page7(){
+      location.replace("new.html");
+     } 
+  
